05a80b74501a3a23e748adf0871da8979d3b3224
andrewsamehkedees <62757067+andrewsamehkedees@users.noreply.github.com>
2024-01-15 20:15:11 +0200

    Update test.js

--- apitesting/tests/test.js
+++ apitesting/tests/test.js
@@ -3,23 +3,24 @@ const assert = require('assert');
 const express = require('express');
 const server = request.agent("http://localhost:3000");
 const app = express();
-//getting the token after using get auth for the user by enter the right email and password
-let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzI4NjksImlhdCI6MTcwNTE3NDg0MSwiZXhwIjoxNzA1MjYxMjQxfQ.oIoGgFJ5sp-B-jrMYPAO8Be72lORdmPka3zOa4t1K5Y';
-let key_admin ='key_admin';
-// Now, let's write the tests
+//i got the token from auth by using postman 
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJAZ21haWwuY29tIiwiaWQiOjg5NzYwLCJpYXQiOjE3MDUzNDIyNDUsImV4cCI6MTcwNTQyODY0NX0._LT-fFz6t6e1bzP-idi68AY9QaRaZZwsoXAmaGk6Y0U';
+const jsonData = {
+  "key_admin": "keyadmin123"
+};
 
 describe('Mock User Auth API', function () {
   // Test authentication route
   describe('authentication test', function () {
     it('should authenticate user with valid credentials', function (done) {
-        server
+      server
         .post('/api/v1/auth')
         .send({ email: 'user@gmail.com', password: 'user123' })
-        .expect(200,done);
+        .expect(200, done);
     });
 
     it('should return an error with invalid credentials', function (done) {
-        server
+      server
         .post('/api/v1/auth')
         .send({ email: 'invalid@gmail.com', password: 'invalid' })
         .expect(401, done);
@@ -29,14 +30,14 @@ describe('Mock User Auth API', function () {
   // Test create user route
   describe('create user test', function () {
     it('should create a user with valid data', function (done) {
-        server
+      server
         .post('/api/v1/users')
-        .send({ name: 'df', email: 'sdf@gmail.com', password: 'asdf2332' })
-        .expect(200,done);
+        .send({ name: 'test1233', email: 'test1233@gmail.com', password: 'test1233' })
+        .expect(200, done);
     });
 
     it('should return an error with invalid data', function (done) {
-        server
+      server
         .post('/api/v1/users')
         .send({ name: '232323', email: 'asasdf', password: 'asdfasdf3' })
         .expect(401, done);
@@ -44,24 +45,24 @@ describe('Mock User Auth API', function () {
   });
   describe('get user test', function () {
     it('should get user with valid authorization', function (done) {
-        server
+      server
         .get('/api/v1/users')
         .set('Authorization', `${token}`)
-        .expect(200,done);
+        .expect(200, done);
     });
 
     it('should return an error without authorization', function (done) {
-        server
+      server
         .get('/api/v1/users')
         .set('Authorization', 'test')
-        .expect(401, done);
+        .expect(403, done);
     });
   });
 
   // Test patch user route
   describe('update user test', function () {
     it('should update user with valid authorization', function (done) {
-        server
+      server
         .patch('/api/v1/users')
         .set('Authorization', `${token}`)
         .send({ name: 'newName' })
@@ -69,40 +70,42 @@ describe('Mock User Auth API', function () {
     });
 
     it('should return an error without authorization', function (done) {
-        server
+      server
         .patch('/api/v1/users')
         .send({ name: 'newName' })
-        .expect(401, done);
+        .expect(403, done);
     });
   });
 
   // Test delete user route
   describe('delete user test', function () {
     it('should delete user with valid authorization', function (done) {
-        server
+      server
         .delete('/api/v1/users')
-        // .set('Authorization', `${token}`)
+        .set('Authorization', `${token}`)
         .expect(200, done);
     });
 
     it('should return an error without authorization', function (done) {
-        server
+      server
         .delete('/api/v1/users')
-        .expect(401, done);
+        .expect(403, done);
     });
   });
   describe('delete all users test', function () {
     it('should delete user with valid authorization', function (done) {
-        server
+      server
         .delete('/api/v1/all-users')
-        .set('key_admin', `${key_admin}`)
+        .send(jsonData)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
         .expect(200, done);
     });
 
     it('should return an error without authorization', function (done) {
-        server
+      server
         .delete('/api/v1/all-users')
-        .expect(401, done);
+        .expect(403, done);
     });
   });
 });
